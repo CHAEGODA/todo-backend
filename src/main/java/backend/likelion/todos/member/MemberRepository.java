535b32ce95bb0df7fabbd6fed4a7e88fa97b8d4a
CHAEGODA <chae_goda@naver.com>
2024-03-10 23:13:26 +0900

    feat : MemberRepository에서 멤버 id를 설정하고 map에 저장하는 save메서드 구현

--- src/main/java/backend/likelion/todos/member/MemberRepository.java
+++ src/main/java/backend/likelion/todos/member/MemberRepository.java
@@ -14,9 +14,11 @@ public class MemberRepository {
     // 멤버를 저장소에 저장하고 저장된 멤버를 반환합니다.
     public Member save(Member member) {
         // TODO [1단계] member의 id를 설정하세요.
+        member.setId(id++);
         // TODO [1단계] members 맵에 member를 추가하세요.
+        members.put(member.getId(),member);
         // TODO [1단계] member를 반환하세요.
-        return null;
+        return member;
     }
 
     // 주어진 id에 해당하는 멤버를 찾아 Optional로 반환합니다.
