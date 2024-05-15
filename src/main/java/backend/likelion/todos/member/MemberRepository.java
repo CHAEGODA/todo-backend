230394fcf8740b4aa328e8e06368c8c901b7384d
CHAEGODA <121277243+CHAEGODA@users.noreply.github.com>
2024-05-15 21:48:47 +0900

    feat : MemberRepository 구현

--- src/main/java/backend/likelion/todos/member/MemberRepository.java
+++ src/main/java/backend/likelion/todos/member/MemberRepository.java
@@ -5,13 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MemberRepository {
-    // TODO [10단계] JpaRepository를 상속받습니다.
+public interface MemberRepository extends JpaRepository<Member, Long> {
 
-    // TODO [10단계] 아래는 기존 코드입니다. 컴파일 오류가 발생하고 있는데, 이를 해결하세요.
-    @Repository
-    public interface MemberRepository extends JpaRepository<Member, Long> {
-
-        Optional<Member> findByUsername(String username);
-    }
+    Optional<Member> findByUsername(String username);
 }
